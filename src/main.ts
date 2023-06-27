372eebd6cdbd925ce79764a8d1f36cad938d10b7
이관영 <stufit@marketboro.com>
2023-06-27 18:05:33 +0900

    apollo studio 적용

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import * as process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT;
+  app.enableCors({
+    origin: 'https://studio.apollographql.com', // or '*' to allow any origin
+    methods: 'POST',
+    credentials: true, // this is necessary if you're using cookies
+  });
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
